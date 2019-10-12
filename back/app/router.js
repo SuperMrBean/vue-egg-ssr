35a382772f59b82d7e18e36572418a37b062dcba
“SuperMrBean <your_registe330178030@qq.com>
2019-10-12 10:15:38 +0800

    'test'

--- back/app/router.js
+++ back/app/router.js
@@ -7,6 +7,7 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.post('/', controller.home.index);
+  // user 用户相关
   router.get('/api/user/info', controller.user.info);
   router.post('/api/user/register', controller.user.register);
 };
