5a5b604b8421f6b852aeeb1dc9435c63de373800
“SuperMrBean <your_registe330178030@qq.com>
2019-10-15 18:03:14 +0800

    'test'

--- back/app/controller/banner.js
+++ back/app/controller/banner.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+
+// 定义创建用户接口的请求参数规则
+const registerRule = {
+  password:'string' || 'number',
+  name:'string' || 'number',
+  telephone:'string' || 'number',
+  email:'string' || 'number'
+};
 class User extends Controller {
   async register(){
     const { ctx } = this
