7d99d97ce14b4cff42fc042063f3ddc0e6a8efe8
“SuperMrBean <your_registe330178030@qq.com>
2019-12-30 15:28:17 +0800

    'test'

--- front/utils/api.js
+++ front/utils/api.js
@@ -1,20 +1,20 @@
 import http from '@/plugins/http'
 function getBanner() {
   return http({
-    url: 'http://127.0.0.1:7001/web/banner/getList',
+    url: 'http://www.wxdriver.com/web/banner/getList',
     method: 'get'
   })
 }
 function getArticleList(params) {
   return http({
-    url: 'http://127.0.0.1:7001/web/list/getList',
+    url: 'http://www.wxdriver.com/web/list/getList',
     method: 'get',
     params
   })
 }
 function getArticle(params) {
   return http({
-    url: 'http://127.0.0.1:7001/web/list/getArticle',
+    url: 'http://www.wxdriver.com/web/list/getArticle',
     method: 'get',
     params
   })
